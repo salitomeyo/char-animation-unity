b9aad90d68550770545d9ecd0058c5bcfe79a010
Salome <salitomeyo16@gmail.com>
2022-01-30 22:18:04 -0500

    feat: movement and rotation with player input

---
+++ Assets/Scripts/Kitty/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 10)]
+    [Tooltip("Velocidad de movimiento del personaje en m/s")]
+    private float speed;
+
+    [SerializeField]
+    [Range(0, 360)]
+    [Tooltip("Velocidad de la rotacion del personaje en grados/s")]
+    private float rotationSpeed; 
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = speed*Time.deltaTime;
+        float rotation = rotationSpeed*Time.deltaTime;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = -Input.GetAxis("Vertical");
+
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 dir = new Vector3(vertical, 0, horizontal);
+        Vector3 angle = new Vector3(0, mouseX, 0);
+
+        transform.Translate(dir.normalized*dist);
+        transform.Rotate(angle.normalized*rotationSpeed);
+
+        // if(Input.GetKey(KeyCode.D))
+        // {
+        //     transform.Translate(0, 0, movement);
+        // }
+        // if(Input.GetKey(KeyCode.A))
+        // {
+        //     transform.Translate(0, 0, -movement);
+        // }
+        // if(Input.GetKey(KeyCode.W))
+        // {
+        //     transform.Translate(-movement, 0, 0);
+        // }
+        // if(Input.GetKey(KeyCode.S))
+        // {
+        //     transform.Translate(movement, 0, 0);
+        // }
+    }
+}
